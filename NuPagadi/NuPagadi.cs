c940d9eb10386a4ce1fe909d90b626c80162bede
tamasii <milkokochev@mail.bg>
2015-02-12 20:35:57 +0200

    proba

---
+++ NuPagadi/NuPagadi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class NuPagadi
+    {
+        static void Main()
+        {
+            // GAME START SCREEN
+            //Draw the field
+            //Draw the eggs
+            //Move the eggs
+            //Move the wolf
+            //Scoring
+
+        }
+    }
